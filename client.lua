53cfa89c7a5f8e47b18cf302d30fe64fbc6c992d
Adi Parmar <parmar.aditya2015@gmail.com>
2021-04-22 23:17:53 -0400

    players get a square when they connect.

--- client.lua
+++ client.lua
@@ -5,6 +5,9 @@ local client = clientServer.client
 client.enabled = true
 client.start('127.0.0.1:22122')
 
+local share = client.share
+local home = client.home
+
 function client.connect()
   print('client: connected to server')
 end
@@ -23,5 +26,15 @@ function client.receive(message, ...)
   if message == 'otherClientPressedKey' then
     local otherClientId, key = ...
     print('client: other client ' .. otherClientId .. " pressed key '" .. key .. "'")
+  end
+end
+
+function client.draw()
+  if client.connected then
+    for clientId, location in pairs(share.players) do
+      love.graphics.rectangle('fill', location.x - 20, location.y - 20, 40, 40)
+    end
+  else
+    love.graphics.print('connecting...', 20, 20)
   end
 end
